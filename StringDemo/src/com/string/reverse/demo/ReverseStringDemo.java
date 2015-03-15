068979741f6ae73df523f9e8302b632f88d29b5c
anshul2goyal <anshul2goyal@gmail.com>
2015-03-15 13:16:40 +0000

    Updated to take the input from user

--- StringDemo/src/com/string/reverse/demo/ReverseStringDemo.java
+++ StringDemo/src/com/string/reverse/demo/ReverseStringDemo.java
@@ -1,5 +1,7 @@
 package com.string.reverse.demo;
 
+import java.util.Scanner;
+
 /**
  * @author Anshul_Goyal
  * 
@@ -10,18 +12,23 @@ public class ReverseStringDemo {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter the string to be reversed...");
+		
+		String str = scanner.nextLine();
 		long startTime = System.currentTimeMillis();
-		String str1 = "Hello World";
 		String reverse = "";
-		for(int i = str1.length()-1; i>=0 ; i--){
-			reverse += str1.charAt(i);
+		for(int i = str.length()-1; i>=0 ; i--){
+			reverse += str.charAt(i);
 		}
 		System.out.printf("The reverse string is :::::%s\n",reverse);
 		long endTime = System.currentTimeMillis();
 		System.out.println("Total Time: "+(endTime - startTime));
 		
+		scanner.close();
 	}
 
 }
