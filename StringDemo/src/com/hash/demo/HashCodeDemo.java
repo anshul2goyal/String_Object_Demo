3d6ead6a0ea637704e5c2c37f549510e65708a22
anshul2goyal <anshul2goyal@gmail.com>
2015-03-15 12:55:54 +0000

    Demo

--- StringDemo/src/com/hash/demo/HashCodeDemo.java
+++ StringDemo/src/com/hash/demo/HashCodeDemo.java
@@ -4,6 +4,7 @@
 package com.hash.demo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * @author Anshul_Goyal
@@ -18,9 +19,25 @@ public class HashCodeDemo {
 	 */
 	public static void main(String[] args) {
 		
-		String str = "2009";
+		/**
+		 * Hashcode is implemented using following:
+		 * s.charAt(0) * 31 ^ n-1 + s.charAt(1) * 31 ^ n-2 + ... + s.charAt(n-1)
+		 * 
+		 * e.g. String str = "ABC";
+		 * 
+		 * int hashcode = "A" * 31 ^ (3-1) + "B" * 31 ^ (3-2) + "C"
+		 * 
+		 * "A", "B", "C" will be converted to the Ascii code so,
+		 * 
+		 * hashcode = 65* 31 ^(3-1) + 66 * 31 ^(3-2) + 67 
+		 *  
+		 * 
+		 *  
+		 * 
+		 */
+		String str = "ABC";
 		System.out.println(str.hashCode());
-		
+		System.out.println("Calculated Hash code is :: "+"Aa".hashCode() + "-----" + "BB".hashCode());
 		String str1 = new String("2009");
 		System.out.println(str1.hashCode());
 		
@@ -37,7 +54,16 @@ public class HashCodeDemo {
 		
 		System.out.println("-------------------------------------------");
 		
-		System.out.println("Array List element hash code :: "+arrayList.get(0).hashCode());
+		Iterator<Integer> iterator = arrayList.iterator();
+		
+		System.out.println("Array List element hash code :: "+ iterator.next().hashCode());
+		
+		HashCodeDemo codeDemo = new HashCodeDemo();
+		System.out.println(codeDemo.hashCode());
+		
+		//Negative hash code
+		String obj3 = "199999999999999999999";
+		System.out.println(obj3.hashCode());
 		
 	}
 
